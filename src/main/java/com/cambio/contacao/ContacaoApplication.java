b7a660b14b29948256fbe29c098f20ec83e0298f
Igor ujiie <igorkazuhico@hotmail.com>
2024-09-25 18:47:29 -0300

    initial

---
+++ src/main/java/com/cambio/contacao/ContacaoApplication.java
@@ -0,0 +1,13 @@
+package com.cambio.contacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cambio/contacao/ContacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cambio.contacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
